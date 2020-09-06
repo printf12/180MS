a5c41b3445216957f4436959aa693a90f30d9cbf
Acheche <oussema.acheche@bauerfeind.com>
2020-09-06 13:57:46 +0200

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WhatwedoComponent } from './whatwedo/whatwedo.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WhatwedoComponent,
+    AboutusComponent,
+    PortfolioComponent,
+    ContactComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
